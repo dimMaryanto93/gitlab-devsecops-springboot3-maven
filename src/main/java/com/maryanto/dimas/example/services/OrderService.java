19222f2b5d0b4e71ff024a6bc6d7f3012ffbdbfd
dimasm93 <software.dimas_m@icloud.com>
2023-10-04 11:15:49 +0700

    update coverage test

--- src/main/java/com/maryanto/dimas/example/services/OrderService.java
+++ src/main/java/com/maryanto/dimas/example/services/OrderService.java
@@ -1,8 +1,22 @@
 package com.maryanto.dimas.example.services;
 
+import java.math.BigDecimal;
+
 public class OrderService {
 
-    public String getString(){
+    public String getString() {
         return "new order";
+    }
+
+    public BigDecimal getPrice(Integer qty, BigDecimal price) throws Exception {
+        if (qty <= 0) {
+            throw new Exception("Quantity can't be less than 0");
+        }
+
+        if (price.compareTo(BigDecimal.ZERO) < -1) {
+            throw new Exception("Price can't be less than 0");
+        }
+
+        return price.multiply(BigDecimal.valueOf(qty));
     }
 }
--- src/test/java/com/maryanto/dimas/example/services/OrderServiceTest.java
+++ src/test/java/com/maryanto/dimas/example/services/OrderServiceTest.java
@@ -2,6 +2,8 @@ package com.maryanto.dimas.example.services;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
@@ -11,5 +13,17 @@ class OrderServiceTest {
     public void testGetString() {
         assertEquals(this.service.getString(), "new order");
         assertNotEquals(this.service.getString(), "NEW ORDER");
+    }
+
+    @Test
+    public void testOrderItem() throws Exception {
+        assertEquals(this.service.getPrice(2, new BigDecimal(10000)), new BigDecimal(20000));
+
+        assertThrowsExactly(
+                Exception.class,
+                () -> {
+                    this.service.getPrice(0, new BigDecimal(1000));
+                },
+                "Quantity can't be less than 0");
     }
 }
